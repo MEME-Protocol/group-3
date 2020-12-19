62e0e3e999c33a4b99197e221fa7b9de049e1117
Sebastian Voigt <VoigtSebastian@protonmail.com>
2020-12-19 17:08:27 +0100

    Add testing client

--- ui.py
+++ ui.py
@@ -1,3 +1,16 @@
 #! /usr/bin/python3.9
+from util.common import json_size_struct
+from model.register import Register
+from socket import setdefaulttimeout, socket, SOCK_STREAM, AF_INET, SOCK_DGRAM
+from struct import Struct
 
-print("ui")
+with socket(AF_INET, SOCK_STREAM) as client_socket:
+    tcp_host = "127.0.0.1"
+    tcp_port = 2000
+
+    client_socket.connect((tcp_host, tcp_port))
+    print("Client connected to server")
+
+    json = Register("torsten", "127.0.0.1", 5000).to_json().encode("utf-8")
+
+    client_socket.sendall(json_size_struct.pack(len(json)) + json)
