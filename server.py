5d51ec30cea07c2096f2013f8701f367dc5f7944
Sebastian Voigt <VoigtSebastian@protonmail.com>
2020-12-19 13:22:28 +0100

    Add example json serialization and deserialization

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
 #! /usr/bin/python3.9
+from model.register import Register
+from model.unregister import Unregister
+from model.user_list import UserList, User, AddedRemovedUsers
+
+print(
+    UserList(
+        AddedRemovedUsers(
+            [User("nickname", "127.0.0.1", 0000)],
+            [])
+    ).to_json())
+
+Register.from_json(""" { "type": "register", "nickname": "nickname", "ip": "127.0.0.1", "port": 0 } """)
