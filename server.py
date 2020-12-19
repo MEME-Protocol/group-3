7910d62732003cca5290572b8f10d65caf976c3d
Sebastian Voigt <VoigtSebastian@protonmail.com>
2020-12-19 17:07:49 +0100

    Move server creation out of loop and add logging

--- server.py
+++ server.py
@@ -16,16 +16,18 @@ def wait_for_tcp_connection():
     tcp_host = "0.0.0.0"
     tcp_port = 2000
     with socket(AF_INET, SOCK_STREAM) as tcp_server:
+        tcp_server.bind((tcp_host, tcp_port))
+        tcp_server.listen()
+        print(f"Started tcp server at {tcp_host}:{tcp_port}")
         while not Registrar.shutdown_requested():
-            tcp_server.bind((tcp_host, tcp_port))
-            tcp_server.listen()
-
             connection, _ = tcp_server.accept()
+            print("Server accepted connection")
             TcpServerConnection(connection).start()
+    print("Stopped listening to tcp-connections")
 
 setdefaulttimeout(20)
 
-Thread(target=wait_for_tcp_connection, daemon=True)
+Thread(target=lambda: wait_for_tcp_connection(), daemon=True).start()
 
 signal.signal(signal.SIGINT, shutdown_hook)
 signal.pause()
