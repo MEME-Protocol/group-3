070e43924dba79dfaae593a77a0a482f9fc668bb
Sebastian Voigt <VoigtSebastian@protonmail.com>
2020-12-20 19:29:18 +0100

    Implement tcp listener

--- client.py
+++ client.py
@@ -7,11 +7,7 @@ import signal
 from model.register import Register
 from model.unregister import Unregister
 from util.common import json_size_struct
-
-def listen_on_connection(connection):
-    while True:
-        buffer = connection.recv(4096)
-        print(buffer)
+from model.client.tcp_listener import TcpListener
 
 tcp_host = "127.0.0.1"
 tcp_port = 2000
@@ -19,7 +15,8 @@ tcp_port = 2000
 user_name = get_first_name()
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((tcp_host, tcp_port))
-Thread(target=lambda: listen_on_connection(client_socket), daemon=True).start()
+
+TcpListener(client_socket).start()
 
 json_register = Register(user_name, "127.0.0.1", 5000).to_json().encode("utf-8")
 client_socket.sendall(json_size_struct.pack(len(json_register)) + json_register)
