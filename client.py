209bfe50bd38c12574ce3cd231cc360559a39c42
Sebastian Voigt <VoigtSebastian@protonmail.com>
2020-12-21 10:17:13 +0100

    Add ip addresses to client information

--- client.py
+++ client.py
@@ -10,19 +10,27 @@ from model.client.client_actor import ClientActor
 from model.client.tcp_listener import TcpListener
 from model.register import Register
 from model.unregister import Unregister
+from model.user_list import User
 from util.common import json_size_struct
 
-tcp_host = "127.0.0.1"
-tcp_port = 2000
-
 user_name = input("What is your nickname: ").strip()
-user_port = int(input("What is your favorite port: "))
+user_port = int(input("What is your favorite port: ").strip())
 if user_port < 50000 or user_port > 59999:
     print("The port you choose cannot be used for this application")
     sys.exit(1)
 
+user_ip = input("What is your ip-address (leave empty for 127.0.0.1): ").strip()
+if len(user_ip) == 0:
+    user_ip = "127.0.0.1"
+
+server_port = 2000
+server_host = input("What is the servers ip (leave empty for 127.0.0.1): ").strip()
+if len(server_host) == 0:
+    server_host = "127.0.0.1"
+
 client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((tcp_host, tcp_port))
+client_socket.connect((server_host, server_port))
+
 
 input_actor = InputActor()
 input_actor.start()
@@ -30,7 +38,7 @@ actor = ClientActor(input_actor)
 actor.start()
 TcpListener(client_socket, actor).start()
 
-json_register = Register(user_name, "127.0.0.1", user_port).to_json().encode("utf-8")
+json_register = Register(user_name, user_ip, user_port).to_json().encode("utf-8")
 client_socket.sendall(json_size_struct.pack(len(json_register)) + json_register)
 
 def shutdown_hook(sig, frame):
